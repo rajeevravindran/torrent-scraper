16d38c5f79ab04a1a335265656ceafe2de5b735f
Rajeev Ravindran <razorrajeev@gmail.com>
2016-05-08 13:46:20 +0530

    Input conditioning to 2 digit numbers for season and episode numbers

--- scraper.py
+++ scraper.py
@@ -3,12 +3,12 @@ from lxml import html
 import re
 print 'Enter Season number'
 found=False
-season=input()
+season=format(input(),'02d')
 #season=9
 print 'Enter episode number'
-episode=input()
+episode=format(input(),'02d')
 #episode=18
-search_string='S0'+str(season)+'E'+str(episode)
+search_string='S'+str(season)+'E'+str(episode)
 print '[INFO] Searching ',search_string
 print '[INFO] SENDING REQUEST'
 page = requests.get('https://pirateproxy.pe/search/The%20big%20bang%20theory/0/7/0')
@@ -17,7 +17,7 @@ print '[INFO] DONE'
 tree = html.fromstring(page.content)
 torrent_name=tree.xpath('//*[@id="searchResult"]/tr/td/div/a/text()')
 for i in range(1,30):
-    print 'Searching '+str(search_string)+' in '+str(torrent_name[i])
+    #print 'Searching '+str(search_string)+' in '+str(torrent_name[i])
     match = re.search(str(search_string),torrent_name[i])
     if(match !=None):
         if (match.group(0)==search_string):
@@ -25,11 +25,12 @@ for i in range(1,30):
             print 'Found at node '+str(i)
             found=True
             break
+###
 if(found == True):
     n=i+1
     magnet_xpath='//*[@id="searchResult"]/tr['+str(n)+']/td[2]/a[1]/@href'
     print ' Downloading '+str(torrent_name[n-1])
-    magnet_link = str(tree.xpath(magnet_xpath))
-    print magnet_link
+    magnet_link = tree.xpath(magnet_xpath)
+    print magnet_link[0]
 else:
     print 'Not Found'
