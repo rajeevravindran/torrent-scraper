2478120e2056575b0715419a0903fac1c813ddfb
Rajeev Ravindran <razorrajeev@gmail.com>
2016-05-08 20:20:21 +0530

    Simple Integration with Deluge Web UI. Still need to add modular approach. Preferably object oriented.

--- deluge-json.py
+++ deluge-json.py
@@ -11,9 +11,9 @@ print "---------------"
 
 print "---------------"
 
-link="magnet:?xt=urn:btih:1b43cc1169821dda932868f0b317e48863717ba4&dn=Suits.S05E15.HDTV.x264-KILLERS%5Bettv%5D&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969"
-payload2='{"method": "core.add_torrent_magnet", "params": ["magnet:?xt=urn:btih:16c8076181b17eee1ed8690614b0c23013378f20&dn=House+of+Cards+2013+S04E02+720p+WEBRip+X264-DEFLATE&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969", {}], "id": 2}'
-
+link="\"magnet:?xt=urn:btih:1b43cc1169821dda932868f0b317e48863717ba4&dn=Suits.S05E15.HDTV.x264-KILLERS%5Bettv%5D&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969\""
+payload2=str('{"method": "core.add_torrent_magnet", "params":[')+link+str(', {}], "id": 2}')
+print payload2
 response2 = r.post("http://192.168.0.104:8112/json", data=payload2)
 
 print response2.headers
--- scraper.py
+++ scraper.py
@@ -3,21 +3,21 @@ from lxml import html
 import re
 print 'Enter Season number'
 found=False
-season=format(input(),'02d')
-#season=9
-print 'Enter episode number'
-episode=format(input(),'02d')
-#episode=18
+#season=format(input(),'02d')
+season=format(6,'02d')
+#print 'Enter episode number'
+#episode=format(input(),'02d')
+episode=format(2,'02d')
 search_string='S'+str(season)+'E'+str(episode)
 print '[INFO] Searching ',search_string
 print '[INFO] SENDING REQUEST'
-page = requests.get('https://pirateproxy.pe/search/The%20big%20bang%20theory/0/7/0')
+page = requests.get('https://pirateproxy.pe/search/Game%20of%20Thrones/0/7/0')
 #page = requests.get('http://econpy.pythonanywhere.com/ex/001.html')
 print '[INFO] DONE'
 tree = html.fromstring(page.content)
 torrent_name=tree.xpath('//*[@id="searchResult"]/tr/td/div/a/text()')
 for i in range(1,30):
-    #print 'Searching '+str(search_string)+' in '+str(torrent_name[i])
+    print 'Searching '+str(search_string)+' in '+str(torrent_name[i])
     match = re.search(str(search_string),torrent_name[i])
     if(match !=None):
         if (match.group(0)==search_string):
@@ -32,5 +32,18 @@ if(found == True):
     print ' Downloading '+str(torrent_name[n-1])
     magnet_link = tree.xpath(magnet_xpath)
     print magnet_link[0]
+    link="\""+str(magnet_link[0])+"\""
+    payload='{"method": "auth.login", "params": ["9324651015"], "id": 1}'
+    r=requests.session()
+    response = r.post("http://192.168.0.104:8112/json", data=payload)
+    payload2=str('{"method": "core.add_torrent_magnet", "params":[')+link+str(', {}], "id": 2}')
+    response2 = r.post("http://192.168.0.104:8112/json", data=payload2)
+    print response.headers
+    print response.text
+    print "---------"
+    print response2.headers
+    print response2.text
+    print "---------"
 else:
     print 'Not Found'
+
